5534f960914a8223c0660e68209d436a2bcae108
aflamentTM <aflament@thalasso-saintmalo.com>
2025-01-24 14:37:32 +0100

    first commit

---
+++ table_data_csv_backup.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: CSV Data Backup
+ * Plugin URI: https://github.com/aflamentTM/table_data_csv_backup
+ * Description: This plugin allows you to backup your table data in CSV format.
+ * Version: 1.0
+ * Author: aflamentTM
+ * Author URI:
+ */
