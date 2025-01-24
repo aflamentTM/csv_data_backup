a3bfb5a874f3d353979747796f5705f94fe1e398
aflamentTM <aflament@thalasso-saintmalo.com>
2025-01-24 15:45:53 +0100

    plugin terminé est à travailler

--- table_data_csv_backup.php
+++ table_data_csv_backup.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * Plugin Name: CSV Data Backup
  * Plugin URI: https://github.com/aflamentTM/table_data_csv_backup
@@ -7,3 +8,54 @@
  * Author: aflamentTM
  * Author URI:
  */
+
+//  Plugin Menu in admin
+add_action('admin_menu', 'tdcb_create_admin_menu');
+// Create a page - A Button Export
+//  Export the data in CSV format
+// Add the menu
+function tdcb_create_admin_menu()
+{
+    add_menu_page('CSV Data Backup Plugin', 'CSV Data Backup', 'manage_options', 'csv-data-backup', 'tdcb_export_form', 'dashicons-database-export', 8);
+}
+// Form layout
+function tdcb_export_form()
+{
+    ob_start();
+    include_once plugin_dir_path(__FILE__) . 'template/table_data_backup.php';
+    $layout = ob_get_contents();
+    ob_clean();
+    echo $layout;
+}
+// Export the data in CSV format
+add_action('admin_init', 'tdcb_handle_form_export');
+function tdcb_handle_form_export()
+{
+    if (isset($_POST['tdcb_export_button'])) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'students_data';
+        $results = $wpdb->get_results("SELECT * FROM $table_name", ARRAY_A);
+        if (empty($results)) {
+            echo "No data found";
+            return;
+        }
+        $filename = "students_data" . time() . ".csv";
+        header('Content-Type: text/csv', 'charset=utf-8');
+        header('Content-Disposition: attachment; filename=' . $filename);
+
+        $output = fopen('php://output', 'w');
+        fputcsv($output, array_keys($results[0]));
+        foreach ($results as $row) {
+            fputcsv($output, $row);
+            // foreach ($results as $result) {
+            //     $csv_output .= $result->id . ', ' . $result->name . ', ' . $result->email . ', ' . $result->phone . ', ' . $result->message . "\n";
+            // }
+            // header("Content-type: text/x-csv");
+            // header("Content-Disposition: attachment; filename=table_data.csv");
+            // echo $csv_output;
+            // exit;
+        }
+        fclose($output);
+        exit;
+    }
+}
---
+++ template/table_data_backup.php
@@ -0,0 +1,7 @@
+<div class="wrap">
+    <form action="" method="post" >
+        <h2>Export Data</h2>
+        <?php submit_button('Export Data', 'primary', 'tdcb_export_button'); ?>
+    </form>
+
+</div>
